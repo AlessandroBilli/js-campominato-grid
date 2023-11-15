ace2465dc1c39394797b54477bfbebc789b6cca4
AlessandroBilli <skillmanbungo@gmail.com>
2023-11-15 19:22:05 +0100

    feat: griglia

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,15 @@
 'use strict'
+function generaCelle() {
+    const grigliaDiv = document.querySelector('.griglia');
+  
+    for (let i = 0; i < 100; i++) {
+      const cella = document.createElement('div');
+      cella.classList.add('cella');
+      cella.textContent = i + 1;
+      grigliaDiv.appendChild(cella);
+    }
+  }
+  
+  generaCelle();
+  
+
