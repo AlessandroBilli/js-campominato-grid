d8e23b0e5bbec5e8567f83782c3bd831b8bf54a7
AlessandroBilli <skillmanbungo@gmail.com>
2023-11-15 23:08:53 +0100

    fix . spawn griglia

--- js/myscript.js
+++ js/myscript.js
@@ -1,17 +1,26 @@
 'use strict'
 
 let play = document.getElementById('playButton');
+let clickedOnce = false;
+let grigliaDiv = document.querySelector('.griglia'); 
 
 play.addEventListener('click', function generaCelle() {
-  const grigliaDiv = document.querySelector('.griglia');
-  
-  for (let i = 0; i < 100; i++) {
-    const cella = document.createElement('div');
-    cella.classList.add('cella');
-    cella.textContent = i + 1;
-    grigliaDiv.appendChild(cella);
-  }
+    if (!clickedOnce) {
+        for (let i = 0; i < 100; i++) {
+            const cella = document.createElement('div');
+            cella.classList.add('cella');
+            cella.textContent = i + 1;
+            grigliaDiv.appendChild(cella);
+        }
+        clickedOnce = true;
+    } else {
+        while (grigliaDiv.firstChild) {
+            grigliaDiv.removeChild(grigliaDiv.firstChild);
+        }
+        clickedOnce = false;
+    }
 });
+
 
 
 
